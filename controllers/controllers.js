72950995582b79dacc59a1f08a0a91a9c9c1d5eb
verdun966 <dverdun978@gmail.com>
2022-06-03 20:04:42 -0300

    se modifico las crear jugador

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,29 +1,15 @@
-const {JugadorB} = require('../models/jugador')
-
-const vistaUno = (req, res)=>{
-    res.render('index', { title: 'Express' });
-}
+const {Jugador} = require('../models/jugador')
 
 const vistaJugador = async (req, res) =>{
-    const Tevez = await JugadorB.find()
-    res.json({Tevez})
-}
-
-const otroJugador = async (req, res)=>{
-    try {
-        const guardar = new JugadorB(req.body)
-        await guardar.save()
-        res.status(201).json(save)
-    } catch (err) {
-        res.status(501).json({msg: "no se puede guardar el usuario"})
-    }
+    const jugadores = await Jugador.findById()
+    res.json({jugadores})
 }
 
 const crearJugador = async (req, res)=>{
     try {
-        const save = new JugadorB(req.body)
-        await save.save()
-        res.status(201).json(save)
+        const nuevoJugador = new Jugador(req.body)
+        await nuevoJugador.save()
+        res.status(201).json(nuevoJugador)
     } catch (err) {
         res.status(501).json({msg: "no se puede guardar el usuario"})
     }

b05577b36dd1547e379b55b4a75da2b2bf587d1c
verdun966 <dverdun978@gmail.com>
2022-06-04 18:42:45 -0300

    se elimino app.use

--- app.js
+++ app.js
@@ -15,8 +15,6 @@ app.use(express.json());
 app.use(cors())
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
 
 app.use('/api', apiRouter);
 dbConnection()

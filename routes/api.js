bb13cd93357d1812beb36d77e2aed7611dff0bf2
verdun966 <dverdun978@gmail.com>
2022-07-15 14:46:03 -0300

    se agrego editar y eliminar jugador

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router(); 
-const {vistaJugador, crearJugador} = require('../controllers/controllers.js')
+const {vistaJugador, crearJugador, editarJugador, eliminarJugador} = require('../controllers/controllers.js')
 
 /* GET users listing. */
 router.get('/ver', vistaJugador);
 router.post('/crear', crearJugador);
+router.put('/editar', editarJugador);
+router.delete('/eliminar', eliminarJugador);
  
 module.exports = router;
